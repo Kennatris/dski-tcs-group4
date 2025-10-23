66ef1e6a53e975358c8ddce9027a3d47cf5428b3
Kennatris <tino.st.2004@gmail.com>
2025-10-23 09:28:53 +0200

    Rebase to the current local version.

--- SortingAlgorithmes/TimSort.java
+++
@@ -1,9 +0,0 @@
-// code by diptangsu from https://github.com/diptangsu/Sorting-Algorithms/tree/master
-import java.util.Arrays;
-
-public class TimSort {
-
-    public static void sort(int[] arr) {
-        Arrays.sort(arr); // Java 7+ uses TimSort internally
-    }
-}
